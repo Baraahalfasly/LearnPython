080ce13219ccacb94d3ca1669621fc3c105bc36b
Baraah Alfasly <1029476@rochesterschools.org>
2024-09-08 15:48:30 -0500

    Create check_first_char.py

---
+++ check_first_char.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+name[0]
+
+if name[0] == "B":
+  print("the first letter is B")
+        
+elif name[0] == "A":
+  print("the first letter is A")
+        
+
+else:
+  print("the first letter is not B")

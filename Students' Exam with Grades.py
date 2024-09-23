7b7bf587bec3188aaac628d9952d9a4f8284f00d
Baraah Alfasly <1029476@rochesterschools.org>
2024-09-22 21:43:53 -0500

    Create Students' Exam with Grades.py

---
+++ Students' Exam with Grades.py
@@ -0,0 +1,26 @@
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+questions = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
+
+question_objects = [
+    Question(questions[0], "a"),
+    Question(questions[1], "c"),
+    Question(questions[2], "b")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(len(questions)) + " correct")
+
+run_test(question_objects)
